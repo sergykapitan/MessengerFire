333beb6f32a3a2bad093840a3260aa622686da6e
sergykapitan <12kaptan12@gmail.com>
2020-10-31 13:43:11 +0300

    create branch testing

--- MessengerFire/ViewController.swift
+++ MessengerFire/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController  {
+   
+    
+    private let myArray: NSArray = ["First","Second","Third"]
+    private var myTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
+
+                myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight - barHeight))
+                myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+                myTableView.dataSource = self
+                myTableView.delegate = self
+                self.view.addSubview(myTableView)
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource  {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        myArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        cell.textLabel!.text = "\(myArray[indexPath.row])"
+        return cell
+    }
+    
+    
+}
